c3ca16e3316a1dece61a8e95c1522659edc2d15b
Art <Art>
2019-04-11 10:28:31 +0200

    build docs

---
+++ docs/precache-manifest.35aeef451fb72ab27f96d86e5945d868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3791d3205dd2321a7655",
+    "url": "/lesson-2/static/js/runtime~main.4b3422fa.js"
+  },
+  {
+    "revision": "4794298f14e0aaabef6a",
+    "url": "/lesson-2/static/js/main.01ad4bca.chunk.js"
+  },
+  {
+    "revision": "bf7e99a1b32667cc2658",
+    "url": "/lesson-2/static/js/2.6631d05e.chunk.js"
+  },
+  {
+    "revision": "4794298f14e0aaabef6a",
+    "url": "/lesson-2/static/css/main.1c4f972d.chunk.css"
+  },
+  {
+    "revision": "bf7e99a1b32667cc2658",
+    "url": "/lesson-2/static/css/2.ff2b4582.chunk.css"
+  },
+  {
+    "revision": "8d02feb22b63f0b692dcc443db61fcc9",
+    "url": "/lesson-2/index.html"
+  }
+];
